f2a355e772801067a3143093daa5c2caf35e7216
mr2079 <mohammadalikhoshdooni@yahoo.com>
2023-07-10 00:04:49 +0330

    Add unit of work

---
+++ UnitOfWork/Architecture/Infrastructure/Implementations/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using UnitOfWork.Architecture.Application.Contracts.Persistence;
+using UnitOfWork.Architecture.Infrastructure.Implementations.Repositories;
+using UnitOfWork.Architecture.Infrastructure.Persistence;
+
+namespace UnitOfWork.Architecture.Infrastructure.Implementations.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private IMainDbContext _dbContext;
+    private bool _disposed;
+
+    public UnitOfWork(IMainDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IBaseRepository BaseRepository()
+    {
+        return new BaseRepository(_dbContext);
+    }
+
+    public Task<int> CommitAsync(CancellationToken cancellationToken)
+    {
+        return _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+            if (disposing)
+                _dbContext.Dispose();
+
+        _disposed = true;
+    }
+
+    ~UnitOfWork()
+    {
+        Dispose(false);
+    }
+}
---
+++ UnitOfWork/Architecture/Infrastructure/Persistence/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using UnitOfWork.Architecture.Application.Contracts.Persistence;
+
+namespace UnitOfWork.Architecture.Infrastructure.Persistence;
+
+public interface IUnitOfWork : IDisposable
+{
+    IBaseRepository BaseRepository();
+    Task<int> CommitAsync(CancellationToken cancellationToken);
+}
