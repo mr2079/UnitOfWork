17b9ccfae7fe9aa29c44d58f67fa39868b8714f5
mr2079 <mohammadalikhoshdooni@yahoo.com>
2023-07-10 20:25:49 +0330

    Add person validator

---
+++ UnitOfWork/Architecture/Application/Validators/PersonValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using UnitOfWork.Architecture.Domain.Entities;
+
+namespace UnitOfWork.Architecture.Application.Validators;
+
+public class PersonValidator : AbstractValidator<Person>
+{
+	public PersonValidator()
+	{
+		RuleFor(p => p.FirstName)
+			.NotEmpty().WithMessage("فیلد نام الزامی می باشد");
+
+        RuleFor(p => p.LastName)
+            .NotEmpty().WithMessage("فیلد نام خانوادگی الزامی می باشد");
+    }
+}
--- UnitOfWork/Program.cs
+++ UnitOfWork/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using UOW = UnitOfWork.Architecture.Infrastructure.Implementations.UnitOfWork.UnitOfWork;
 using UnitOfWork.Architecture.Infrastructure.Persistence;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,12 @@ builder.Services.AddControllers()
     .AddNewtonsoftJson(options =>
     {
         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    })
+    .AddFluentValidation(options =>
+    {
+        options.AutomaticValidationEnabled = true;
+        options.RegisterValidatorsFromAssemblyContaining<Program>();
+        options.DisableDataAnnotationsValidation = true;
     });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
